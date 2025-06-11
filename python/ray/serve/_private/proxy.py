eicherseiji/ray

commit ab2f988b99852bb9c377664f18af2aa4fd45ff44

Code refactoring in proxy (#53644)
Signed-off-by: abrar <abrar@anyscale.com>

python/ray/serve/_private/proxy.py
@@ -17,7 +17,6 @@ from starlette.types import Receive
 
 import ray
 from ray._common.utils import get_or_create_event_loop
-from ray.exceptions import RayActorError, RayTaskError
 from ray.serve._private.common import (
     DeploymentID,
     EndpointInfo,
@@ -39,11 +38,17 @@ from ray.serve._private.constants import (
     SERVE_NAMESPACE,
 )
 from ray.serve._private.default_impl import get_proxy_handle
-from ray.serve._private.grpc_util import start_grpc_server
+from ray.serve._private.grpc_util import (
+    get_grpc_response_status,
+    set_grpc_code_and_details,
+    start_grpc_server,
+)
 from ray.serve._private.http_util import (
     MessageQueue,
     convert_object_to_asgi_messages,
+    get_http_response_status,
     receive_http_body,
+    send_http_response_on_exception,
     start_asgi_http_server,
     validate_http_proxy_callback_return,
 )
@@ -74,7 +79,6 @@ from ray.serve._private.utils import (
     is_grpc_enabled,
 )
 from ray.serve.config import HTTPOptions, gRPCOptions
-from ray.serve.exceptions import BackPressureError, DeploymentUnavailableError
 from ray.serve.generated.serve_pb2 import HealthzResponse, ListApplicationsResponse
 from ray.serve.handle import DeploymentHandle
 from ray.serve.schema import LoggingConfig
@@ -82,8 +86,6 @@ from ray.util import metrics
 
 logger = logging.getLogger(SERVE_LOGGER_NAME)
 
-TIMEOUT_ERROR_CODE = "408"
-DISCONNECT_ERROR_CODE = "499"
 SOCKET_REUSE_PORT_ENABLED = (
     os.environ.get("SERVE_SOCKET_REUSE_PORT_ENABLED", "1") == "1"
 )
@@ -582,20 +584,6 @@ class gRPCProxy(GenericProxy):
         )
 
     def service_handler_factory(self, service_method: str, stream: bool) -> Callable:
-        def set_grpc_code_and_details(
-            context: grpc._cython.cygrpc._ServicerContext, status: ResponseStatus
-        ):
-            # Only the latest code and details will take effect. If the user already
-            # set them to a truthy value in the context, skip setting them with Serve's
-            # default values. By default, if nothing is set, the code is 0 and the
-            # details is "", which both are falsy. So if the user did not set them or
-            # if they're explicitly set to falsy values, such as None, Serve will
-            # continue to set them with our default values.
-            if not context.code():
-                context.set_code(status.code)
-            if not context.details():
-                context.set_details(status.message)
-
         async def unary_unary(
             request_proto: Any, context: grpc._cython.cygrpc._ServicerContext
         ) -> bytes:
@@ -710,38 +698,8 @@ class gRPCProxy(GenericProxy):
                 yield result
 
             status = ResponseStatus(code=grpc.StatusCode.OK)
-        except TimeoutError:
-            message = f"Request timed out after {self.request_timeout_s}s."
-            logger.warning(message)
-            status = ResponseStatus(
-                code=grpc.StatusCode.DEADLINE_EXCEEDED,
-                is_error=True,
-                message=message,
-            )
-        except asyncio.CancelledError:
-            message = f"Client for request {request_id} disconnected."
-            logger.info(message)
-            status = ResponseStatus(
-                code=grpc.StatusCode.CANCELLED,
-                is_error=True,
-                message=message,
-            )
-        except BackPressureError as e:
-            status = ResponseStatus(
-                code=grpc.StatusCode.UNAVAILABLE,
-                is_error=True,
-                message=e.message,
-            )
-        except Exception as e:
-            if isinstance(e, (RayActorError, RayTaskError)):
-                logger.warning(f"Request failed: {e}", extra={"log_to_stderr": False})
-            else:
-                logger.exception("Request failed due to unexpected error.")
-            status = ResponseStatus(
-                code=grpc.StatusCode.INTERNAL,
-                is_error=True,
-                message=str(e),
-            )
+        except BaseException as e:
+            status = get_grpc_response_status(e, self.request_timeout_s, request_id)
 
         # The status code should always be set.
         assert status is not None
@@ -1017,49 +975,12 @@ class HTTPProxy(GenericProxy):
 
                     yield asgi_message
                     response_started = True
-        except TimeoutError:
-            status = ResponseStatus(
-                code=TIMEOUT_ERROR_CODE,
-                is_error=True,
-            )
-            logger.warning(f"Request timed out after {self.request_timeout_s}s.")
-            # We should only send timeout response if we have not sent
-            # any messages to the client yet. Header (including status code)
-            # messages can only be sent once.
-            if not response_started:
-                for message in convert_object_to_asgi_messages(
-                    f"Request {request_id} timed out after {self.request_timeout_s}s.",
-                    status_code=408,
-                ):
-                    yield message
-        except asyncio.CancelledError:
-            status = ResponseStatus(
-                code=DISCONNECT_ERROR_CODE,
-                is_error=True,
-            )
-            logger.info(
-                f"Client for request {request_id} disconnected, cancelling request."
-            )
-        except (BackPressureError, DeploymentUnavailableError) as e:
-            status = ResponseStatus(
-                code="503",
-                is_error=True,
-                message=e.message,
-            )
-            if isinstance(e, RayTaskError):
-                logger.warning(f"Request failed: {e}", extra={"log_to_stderr": False})
-            else:
-                for message in convert_object_to_asgi_messages(e.message, 503):
-                    yield message
-        except Exception as e:
-            if isinstance(e, (RayActorError, RayTaskError)):
-                logger.warning(f"Request failed: {e}", extra={"log_to_stderr": False})
-            else:
-                logger.exception("Request failed due to unexpected error.")
-            status = ResponseStatus(
-                code="500",
-                is_error=True,
-            )
+        except BaseException as e:
+            status = get_http_response_status(e, self.request_timeout_s, request_id)
+            for asgi_message in send_http_response_on_exception(
+                status, response_started
+            ):
+                yield asgi_message
 
         finally:
             # For websocket connection, queue receive task is done when receiving

python/ray/serve/_private/grpc_util.py
@@ -1,13 +1,19 @@
 import asyncio
+import logging
 from typing import Callable, List, Optional, Sequence, Tuple
 from unittest.mock import Mock
 
 import grpc
 from grpc.aio._server import Server
 
-from ray.serve._private.constants import DEFAULT_GRPC_SERVER_OPTIONS
+from ray.exceptions import RayActorError, RayTaskError
+from ray.serve._private.constants import DEFAULT_GRPC_SERVER_OPTIONS, SERVE_LOGGER_NAME
+from ray.serve._private.proxy_request_response import ResponseStatus
 from ray.serve.config import gRPCOptions
+from ray.serve.exceptions import BackPressureError, DeploymentUnavailableError
 from ray.serve.generated.serve_pb2_grpc import add_RayServeAPIServiceServicer_to_server
+
+logger = logging.getLogger(SERVE_LOGGER_NAME)
 
 
 class gRPCGenericServer(Server):
@@ -97,3 +103,55 @@ async def start_grpc_server(
 
     await server.start()
     return event_loop.create_task(server.wait_for_termination())
+
+
+def get_grpc_response_status(
+    exc: BaseException, request_timeout_s: float, request_id: str
+) -> ResponseStatus:
+    if isinstance(exc, TimeoutError):
+        message = f"Request timed out after {request_timeout_s}s."
+        return ResponseStatus(
+            code=grpc.StatusCode.DEADLINE_EXCEEDED,
+            is_error=True,
+            message=message,
+        )
+    elif isinstance(exc, asyncio.CancelledError):
+        message = f"Client for request {request_id} disconnected."
+        return ResponseStatus(
+            code=grpc.StatusCode.CANCELLED,
+            is_error=True,
+            message=message,
+        )
+    elif isinstance(exc, (BackPressureError, DeploymentUnavailableError)):
+        if isinstance(exc, RayTaskError):
+            logger.warning(f"Request failed: {exc}", extra={"log_to_stderr": False})
+        return ResponseStatus(
+            code=grpc.StatusCode.UNAVAILABLE,
+            is_error=True,
+            message=exc.message,
+        )
+    else:
+        if isinstance(exc, (RayActorError, RayTaskError)):
+            logger.warning(f"Request failed: {exc}", extra={"log_to_stderr": False})
+        else:
+            logger.exception("Request failed due to unexpected error.")
+        return ResponseStatus(
+            code=grpc.StatusCode.INTERNAL,
+            is_error=True,
+            message=str(exc),
+        )
+
+
+def set_grpc_code_and_details(
+    context: grpc._cython.cygrpc._ServicerContext, status: ResponseStatus
+):
+    # Only the latest code and details will take effect. If the user already
+    # set them to a truthy value in the context, skip setting them with Serve's
+    # default values. By default, if nothing is set, the code is 0 and the
+    # details is "", which both are falsy. So if the user did not set them or
+    # if they're explicitly set to falsy values, such as None, Serve will
+    # continue to set them with our default values.
+    if not context.code():
+        context.set_code(status.code)
+    if not context.details():
+        context.set_details(status.message)

python/ray/serve/_private/http_util.py
@@ -20,11 +20,17 @@ from uvicorn.config import Config
 from uvicorn.lifespan.on import LifespanOn
 
 from ray._private.pydantic_compat import IS_PYDANTIC_2
+from ray.exceptions import RayActorError, RayTaskError
 from ray.serve._private.common import RequestMetadata
 from ray.serve._private.constants import SERVE_HTTP_REQUEST_ID_HEADER, SERVE_LOGGER_NAME
+from ray.serve._private.proxy_request_response import ResponseStatus
 from ray.serve._private.utils import generate_request_id, serve_encoders
 from ray.serve.config import HTTPOptions
-from ray.serve.exceptions import RayServeException
+from ray.serve.exceptions import (
+    BackPressureError,
+    DeploymentUnavailableError,
+    RayServeException,
+)
 
 logger = logging.getLogger(SERVE_LOGGER_NAME)
 
@@ -645,3 +651,52 @@ async def start_asgi_http_server(
     server.install_signal_handlers = lambda: None
 
     return event_loop.create_task(server.serve(sockets=[sock]))
+
+
+def get_http_response_status(
+    exc: BaseException, request_timeout_s: float, request_id: str
+) -> ResponseStatus:
+    if isinstance(exc, TimeoutError):
+        return ResponseStatus(
+            code=408,
+            is_error=True,
+            message=f"Request {request_id} timed out after {request_timeout_s}s.",
+        )
+
+    elif isinstance(exc, asyncio.CancelledError):
+        message = f"Client for request {request_id} disconnected, cancelling request."
+        logger.info(message)
+        return ResponseStatus(
+            code=499,
+            is_error=True,
+            message=message,
+        )
+    elif isinstance(exc, (BackPressureError, DeploymentUnavailableError)):
+        if isinstance(exc, RayTaskError):
+            logger.warning(f"Request failed: {exc}", extra={"log_to_stderr": False})
+        return ResponseStatus(
+            code=503,
+            is_error=True,
+            message=exc.message,
+        )
+    else:
+        if isinstance(exc, (RayActorError, RayTaskError)):
+            logger.warning(f"Request failed: {exc}", extra={"log_to_stderr": False})
+        else:
+            logger.exception("Request failed due to unexpected error.")
+        return ResponseStatus(
+            code=500,
+            is_error=True,
+            message=str(exc),
+        )
+
+
+def send_http_response_on_exception(
+    status: ResponseStatus, response_started: bool
+) -> List[Message]:
+    if response_started or status.code not in (408, 503):
+        return []
+    return convert_object_to_asgi_messages(
+        status.message,
+        status_code=status.code,
+    )

python/ray/serve/tests/conftest.py
@@ -109,10 +109,11 @@ def _shared_serve_instance():
         _system_config={"metrics_report_interval_ms": 1000, "task_retry_delay_ms": 50},
     )
     serve.start(
-        http_options={"host": "0.0.0.0"},
+        http_options={"host": "0.0.0.0", "request_timeout_s": 10},
         grpc_options={
             "port": 9000,
             "grpc_servicer_functions": TEST_GRPC_SERVICER_FUNCTIONS,
+            "request_timeout_s": 10,
         },
     )
     yield _get_global_client()

python/ray/serve/tests/test_controller.py
@@ -125,10 +125,11 @@ def test_get_serve_instance_details_json_serializable(serve_instance, policy):
                 "log_file_path": controller_details.log_file_path,
             },
             "proxy_location": "HeadOnly",
-            "http_options": {"host": "0.0.0.0"},
+            "http_options": {"host": "0.0.0.0", "request_timeout_s": 10.0},
             "grpc_options": {
                 "port": 9000,
                 "grpc_servicer_functions": TEST_GRPC_SERVICER_FUNCTIONS,
+                "request_timeout_s": 10.0,
             },
             "proxies": {
                 node_id: {

commit e56ca722ccc28659a58dbfa4df5ff5657435c5b9

fix serve tests caused due to code refactor (#1837)
this issue is caused due to refactoring introduced in
https://github.com/ray-project/ray/pull/53644

---------

Signed-off-by: abrar <abrar@anyscale.com>
Co-authored-by: Lonnie Liu <95255098+aslonnie@users.noreply.github.com>

python/ray/serve/_private/proxy.py
@@ -1071,6 +1071,8 @@ class HTTPProxy(GenericProxy):
                         code="1000",  # [Sihan] is there a better code for this?
                         is_error=True,
                     )
+            else:
+                status_code = status.code
 
             del self.asgi_receive_queues[internal_request_id]
 