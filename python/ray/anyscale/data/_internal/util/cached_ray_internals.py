a4d4d0598306fc99dbf0188e64929c37c42b194f
iamjustinhsu <140442892+iamjustinhsu@users.noreply.github.com>
2025-07-30 17:23:20 -0700

    [data] add 5s buffer to deadline (#2065)
    If we submit a task immediately before the deadline, Ray Core might not
    have enough time to launch the task and fetch objects before the node is
    terminated. To avoid this, we stop using these inputs some time before
    the deadline.

--- python/ray/anyscale/data/_internal/util/cached_ray_internals.py
+++ python/ray/anyscale/data/_internal/util/cached_ray_internals.py
@@ -6,6 +6,12 @@ from ray.data._internal.execution.node_trackers.actor_location import (
     get_or_create_actor_location_tracker,
 )
 
+# If we submit a task immediately before the deadline,
+# Ray Core might not have enough time to launch the
+# task and fetch objects before the node is terminated.
+# To avoid this, we stop using these inputs some time before the deadline.
+DRAIN_DEADLINE_BUFFER_TIME_MS = 5000
+
 
 @timed_cache(ttl=60)
 def get_local_ongoing_lineage_reconstruction_tasks():
@@ -35,5 +41,5 @@ def get_drained_nodes() -> Set[str]:
         node_id
         for node_id, deadline in get_draining_nodes().items()
         # deadline is in ms
-        if deadline < now * 1000
+        if deadline - DRAIN_DEADLINE_BUFFER_TIME_MS < now * 1000
     }
