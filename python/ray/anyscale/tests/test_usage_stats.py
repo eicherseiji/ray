3814b906e858d4576c79b353e6d06947b14bc6a1
Jiajun Yao <jeromeyjj@gmail.com>
2025-07-31 21:33:19 -0700

    [Core] Update moved modules (#2080)
    Signed-off-by: Jiajun Yao <jeromeyjj@gmail.com>

--- python/ray/anyscale/tests/test_usage_stats.py
+++ python/ray/anyscale/tests/test_usage_stats.py
@@ -6,13 +6,13 @@ import pytest
 from jsonschema import validate
 
 import ray
-import ray._private.usage.usage_lib as ray_usage_lib
+import ray._common.usage.usage_lib as ray_usage_lib
 from ray._common.test_utils import wait_for_condition
 from ray.tests.conftest import (  # noqa: F401
     maybe_setup_external_redis,
     ray_start_cluster,
 )
-from ray.tests.test_usage_stats import (  # noqa: F401
+from ray._common.tests.test_usage_stats import (  # noqa: F401
     print_dashboard_log,
     schema,
     start_usage_stats_server,
