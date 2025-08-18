7c2103b2e1e3e04f871a42980cc89c4ac0d8180a
Omkar Kulkarni <omkar@anyscale.com>
2025-08-18 10:44:51 -0700

    E2E fix: test_controller.py, test_controller_recovery.py (#2132)
    Fixed the port usage in assertion as it failed when DI mode is enabled &
    updated suite names.
    
    ---------
    
    Signed-off-by: omkar <omkar@anyscale.com>

--- python/ray/serve/tests/test_controller.py
+++ python/ray/serve/tests/test_controller.py
@@ -113,6 +113,16 @@ def test_get_serve_instance_details_json_serializable(serve_instance, policy_nam
         controller.get_deployment_details.remote("default", "autoscaling_app")
     )
     replica = deployment_details.replicas[0]
+
+    http_port, grpc_port = None, None
+
+    for target_group in details["target_groups"]:
+        if target_group["protocol"] == "HTTP" and target_group["targets"]:
+            http_port = target_group["targets"][0]["port"]
+
+        if target_group["protocol"] == "gRPC" and target_group["targets"]:
+            grpc_port = target_group["targets"][0]["port"]
+
     expected_json = json.dumps(
         {
             "controller_info": {
@@ -219,7 +229,7 @@ def test_get_serve_instance_details_json_serializable(serve_instance, policy_nam
                     "targets": [
                         {
                             "ip": node_ip,
-                            "port": 8000,
+                            "port": http_port,
                             "instance_id": node_instance_id,
                         },
                     ],
@@ -230,7 +240,7 @@ def test_get_serve_instance_details_json_serializable(serve_instance, policy_nam
                     "targets": [
                         {
                             "ip": node_ip,
-                            "port": 9000,
+                            "port": grpc_port,
                             "instance_id": node_instance_id,
                         },
                     ],
