e4e03b8a9ef4a2e616d72fd3cb949064917735a7
akyang-anyscale <alexyang@anyscale.com>
2025-09-03 13:22:57 -0700

    [serve] Support deployment chaining with different `by_reference` options (#2241)
    The following will error since Serve will try to get the object ref of a
    by-value DeploymentResponse.
    ```
    @serve.deployment
    class Outer:
        def __call__(self, x):
            return x
    
    @serve.deployment
    class Inner:
        def __call__(self):
            return 1
            
    x = self.inner.options(_by_reference=False).remote()
    y = self.outer.options(_by_reference=True).remote(x)
    ```
    
    This PR changes the resolving util code to factor in the `_by_reference`
    setting of both the `DeploymentResponse` that needs to be resolved AND
    the handle doing the resolving to correctly determine how to resolve the
    `DeploymentResponse`.
    https://anyscale1.atlassian.net/browse/SERVE-1090
    
    This PR also changes `obj.__await__()` usage to be `await obj` when
    creating the async task that will resolve the arg since Python 3.12
    removed generators from being coroutines.
    https://anyscale1.atlassian.net/browse/SERVE-1089
    
    ---------
    
    Signed-off-by: akyang-anyscale <alexyang@anyscale.com>

--- python/ray/anyscale/serve/tests/test_handle.py
+++ python/ray/anyscale/serve/tests/test_handle.py
@@ -6,6 +6,7 @@ from ray._common.test_utils import SignalActor
 from ray.anyscale.serve._private.replica_result import gRPCReplicaResult
 from ray.serve._private.common import OBJ_REF_NOT_SUPPORTED_ERROR
 from ray.serve._private.replica_result import ActorReplicaResult, ReplicaResult
+from ray.serve.handle import DeploymentHandle
 from ray.serve.tests.conftest import *  # noqa
 from ray.serve.tests.conftest import _shared_serve_instance  # noqa
 
@@ -45,6 +46,75 @@ async def test_by_reference_false_raises_error(serve_instance):
         await response._to_object_ref()
 
     await signal.send.remote()
+
+
+@pytest.mark.parametrize("inner_by_reference", [True, False])
+@pytest.mark.parametrize("outer_by_reference", [True, False])
+def test_compose_deployments_in_app(
+    serve_instance, inner_by_reference, outer_by_reference
+):
+    """Test composing deployment handle refs within a deployment."""
+
+    @serve.deployment
+    class Downstream:
+        def __init__(self, msg: str):
+            self._msg = msg
+
+        def __call__(self, inp1: str, inp2: str):
+            return f"{self._msg}|{inp1}|{inp2}"
+
+    @serve.deployment
+    class Deployment:
+        def __init__(self, handle1: DeploymentHandle, handle2: DeploymentHandle):
+            self._handle1 = handle1.options(_by_reference=outer_by_reference)
+            self._handle2 = handle2.options(_by_reference=inner_by_reference)
+
+        async def __call__(self):
+            result = await self._handle1.remote(
+                self._handle2.remote("hi1", inp2="hi2"),
+                inp2=self._handle2.remote("hi3", inp2="hi4"),
+            )
+            return f"driver|{result}"
+
+    handle = serve.run(
+        Deployment.bind(
+            Downstream.options(name="downstream1").bind("downstream1"),
+            Downstream.options(name="downstream2").bind("downstream2"),
+        ),
+    )
+    assert (
+        handle.remote().result()
+        == "driver|downstream1|downstream2|hi1|hi2|downstream2|hi3|hi4"
+    )
+
+
+@pytest.mark.parametrize("inner_by_reference", [True, False])
+@pytest.mark.parametrize("outer_by_reference", [True, False])
+def test_compose_apps(serve_instance, inner_by_reference, outer_by_reference):
+    """Test composing deployment handle refs outside of a deployment."""
+
+    @serve.deployment
+    class Deployment:
+        def __init__(self, msg: str):
+            self._msg = msg
+
+        def __call__(self, inp1: str, inp2: str):
+            return f"{self._msg}|{inp1}|{inp2}"
+
+    handle1 = serve.run(
+        Deployment.bind("app1"), name="app1", route_prefix="/app1"
+    ).options(_by_reference=outer_by_reference)
+    handle2 = serve.run(
+        Deployment.bind("app2"), name="app2", route_prefix="/app2"
+    ).options(_by_reference=inner_by_reference)
+
+    assert (
+        handle1.remote(
+            handle2.remote("hi1", inp2="hi2"),
+            inp2=handle2.remote("hi3", inp2="hi4"),
+        ).result()
+        == "app1|app2|hi1|hi2|app2|hi3|hi4"
+    )
 
 
 if __name__ == "__main__":
--- python/ray/anyscale/serve/utils.py
+++ python/ray/anyscale/serve/utils.py
@@ -32,6 +32,10 @@ def asyncio_grpc_exception_handler(loop, context):
     loop.default_exception_handler(context)
 
 
+async def await_deployment_response(deployment_response):
+    return await deployment_response
+
+
 async def resolve_deployment_resp_and_ray_objects(
     obj: Any, request_metadata: RequestMetadata
 ):
@@ -44,13 +48,13 @@ async def resolve_deployment_resp_and_ray_objects(
     if isinstance(obj, DeploymentResponseGenerator):
         raise GENERATOR_COMPOSITION_NOT_SUPPORTED_ERROR
     elif isinstance(obj, DeploymentResponse):
-        if request_metadata._by_reference:
+        if request_metadata._by_reference and obj.by_reference:
             # If sending requests by reference, launch async task to
             # convert DeploymentResponse to an object ref
             return asyncio.create_task(obj._to_object_ref())
         else:
             # Otherwise, resolve DeploymentResponse directly to result
-            return asyncio.create_task(obj.__await__())
+            return asyncio.create_task(await_deployment_response(obj))
     elif not request_metadata._by_reference and isinstance(obj, ray.ObjectRef):
         # If the router is sending requests by value (i.e. using gRPC),
         # resolve all Ray objects to mirror Ray behavior
