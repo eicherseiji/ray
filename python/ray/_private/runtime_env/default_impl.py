be622f14de382eced5bc8f304a4887f7006588e5
Jiajun Yao <jeromeyjj@gmail.com>
2025-09-04 15:42:06 -0700

    [Core] Remove azure protocol turbo support since it's already available in OSS (#2256)
    Already supported in OSS: https://github.com/ray-project/ray/pull/53135
    
    Signed-off-by: Jiajun Yao <jeromeyjj@gmail.com>

--- python/ray/_private/runtime_env/default_impl.py
+++ python/ray/_private/runtime_env/default_impl.py
@@ -18,9 +18,3 @@ def get_image_uri_plugin_cls():  # noqa: F811
     from ray.anyscale._private.runtime_env.image_uri import AnyscaleImageURIPlugin
 
     return AnyscaleImageURIPlugin
-
-
-def get_protocols_provider():  # noqa: F811
-    from ray.anyscale._private.runtime_env.protocol import AnyscaleProtocolsProvider
-
-    return AnyscaleProtocolsProvider
