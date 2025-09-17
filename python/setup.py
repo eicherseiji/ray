921592a6a3fc65975552c245ab10c10e9dc21255
elliot-barn <elliot.barnwell@anyscale.com>
2025-09-17 03:27:10 +0000

    removing requirement
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- python/setup.py
+++ python/setup.py
@@ -229,7 +229,6 @@ if setup_spec.type == SetupType.RAY:
         "pyarrow >= 9.0.0",
     ]
     pydantic_dep = "pydantic!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,<3"
-    polars_dep = "polars>=1.30.0,<2.0.0"
     setup_spec.extras = {
         "cgraph": [
             "cupy-cuda12x; sys_platform != 'darwin'",
@@ -243,7 +242,6 @@ if setup_spec.type == SetupType.RAY:
         "data": [
             numpy_dep,
             pandas_dep,
-            polars_dep,
             *pyarrow_deps,
             "fsspec",
         ],
