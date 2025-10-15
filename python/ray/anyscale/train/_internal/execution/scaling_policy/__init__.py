e59a7ea5b757d8c143489d8b368f11a150b5bde4
Jason Li <57246540+JasonLi1909@users.noreply.github.com>
2025-10-14 17:49:33 -0700

    [Train] Add circular dependency linter pre-commit check to rayturbo train (#2520)
    This PR extends Ray Train's pre-commit circular import check to Rayturbo
    Train.
    
    The analogous OSS PR can be found at
    https://github.com/ray-project/ray/pull/56921.
    
    This PR: 
    - Adds a pre-commit check that uses Ray Train's OSS circular import
    linter to detect circular imports between Ray Train and Rayturbo Train
    directories
    - Adds tests for previously known circular imports 
    - Resolves existing circular imports by applying fixes prescribed by the
    linter
    
    ---------
    
    Signed-off-by: JasonLi1909 <jasli1909@gmail.com>

--- python/ray/anyscale/train/_internal/execution/scaling_policy/__init__.py
+++ python/ray/anyscale/train/_internal/execution/scaling_policy/__init__.py
@@ -1,3 +1,6 @@
+# This is a workaround to avoid a circular import.
+import ray.train.v2._internal.execution.scaling_policy  # noqa: F401
+
 from .elastic import ElasticScalingPolicy
 from .factory import create_scaling_policy
 
