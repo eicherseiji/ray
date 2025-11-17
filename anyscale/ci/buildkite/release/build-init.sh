900d29edbb32a63d1346e1c2209346664db890eb
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2025-11-17 10:59:19 -0800

    [release test] use uv python to run launcher (#2706)
    so that it does not depend on the python version on buildkite ami
    
    ---------
    
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>

--- anyscale/ci/buildkite/release/build-init.sh
+++ anyscale/ci/buildkite/release/build-init.sh
@@ -21,13 +21,31 @@ echo "RAYCI_BUILD_ID: ${RAYCI_BUILD_ID}"
 
 aws ecr get-login-password --region us-west-2 | \
     docker login --username AWS --password-stdin 830883877497.dkr.ecr.us-west-2.amazonaws.com
+
+echo "--- Install Bazel"
+curl -sSfLo /tmp/bazel https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-amd64
+chmod +x /tmp/bazel
+
+
+echo "--- Install uv"
+
+UV_PYTHON_VERSION=3.9
+curl -LsSf https://astral.sh/uv/install.sh | sh
+UV_BIN="${HOME}/.local/bin/uv"
+"${UV_BIN}" python install "${UV_PYTHON_VERSION}"
+UV_PYTHON_BIN="$("${UV_BIN}" python find --no-project "${UV_PYTHON_VERSION}")"
+
+
 echo "--- Generate custom build steps"
 
-curl -sSfLo /tmp/bazelisk https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-amd64
-chmod +x /tmp/bazelisk
+/tmp/bazel build --python_path="${UV_PYTHON_BIN}" \
+  --build_python_zip --enable_runfiles \
+  --incompatible_use_python_toolchains=false \
+  //release:custom_image_build_and_test_init
+
 
 # Keep in sync with test-init.sh
-/tmp/bazelisk run //release:custom_image_build_and_test_init -- \
+BUILD_WORKSPACE_DIRECTORY="${PWD}" bazel-bin/release/custom_image_build_and_test_init \
     --test-collection-file release/release_runtime_tests.yaml \
     --test-collection-file release/release_data_tests.yaml \
     --test-collection-file release/release_multimodal_inference_benchmarks_tests.yaml \
